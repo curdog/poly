49eb3ca8d8c4b4c0e222cdb6091858ed7b84774d
Max <conroy316@yahoo.com>
2013-04-24 18:44:13 -0300

    Update poly.cpp

--- poly.cpp
+++ poly.cpp
@@ -13,10 +13,19 @@
   
  }
  Poly::Poly operator+(const Poly& left, const Poly& right){
+   Poly temp;
+   temp.x2 = left.x2 + right.x2;
+   temp.x = left.x + right.x;
+   temp.one = left.one + right.one;
+   return temp;
    
  }
  Poly::Poly operator-(const Poly& left, const Poly& right){
-   
+   Poly temp;
+   temp.x2 = left.x2 - right.x2;
+   temp.x = left.x - right.x;
+   temp.one = left.one - right.one;
+   return temp;
  }
 
 
