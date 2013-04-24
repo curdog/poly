c08d656a9b91b1723db18c55010959ed93c3a9c7
Max <conroy316@yahoo.com>
2013-04-24 18:20:56 -0300

    Update poly.cpp

--- poly.cpp
+++ poly.cpp
@@ -1,5 +1,23 @@
+#include <iostream>
 #include "poly.h"
 
+ Poly::istream& operator>>(istream& inp, Poly& p1){
+   int temp;
+   getline(inp,temp);
+   int find(string& str)
+   
+   
+ }
+ Poly::ostream& operator<<(ostream& outp, const Poly& p1){
+   
+  
+ }
+ Poly::Poly operator+(const Poly& left, const Poly& right){
+   
+ }
+ Poly::Poly operator-(const Poly& left, const Poly& right){
+   
+ }
 
 
 
